da9720661a0b746334a14542e5cd0e061f574be8
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-01-21 17:05:52 +0900

    Change script.js Split into 3->5 Bottom Bar 20%->15%

--- js/script.js
+++ js/script.js
@@ -214,8 +214,8 @@ window.addEventListener("load",()=>{
 	canv.color(bgColor)
 		.rect(0,0,canv.size().width, canv.size().height)  // 全体を#222で塗りつぶし
 		.color("#555")
-		.rect(0,canv.p2x(80,"y"),canv.size().width,canv.p2x(20,"y"))  // 下の部分 20%
-		.split("x",3,canv.p2x(80,"y"),canv.p2x(20,"y"));  // 下部分を3分割
+		.rect(0,canv.p2x(85,"y"),canv.size().width,canv.p2x(15,"y"))  // 下の部分
+		.split("x",5,canv.p2x(85,"y"),canv.p2x(15,"y"));  // 下部分を3分割
 
 	window.canv = canv;
 });
