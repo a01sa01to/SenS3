73665d1cfb0809d83f7016aecd30b6b5e437eb00
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-01-21 23:53:24 +0900

    Update main.js - Add animation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {sleep, waitUntil} from "./function";
 import {bgColor} from "./variables";
 import {Canvas, Block} from "./class";
-
+const $ = require("jquery");
 
 window.addEventListener("load",()=>{
 	const canv = new Canvas();
@@ -13,4 +13,25 @@ window.addEventListener("load",()=>{
 		.split("x",5,canv.p2x(85,"y"),canv.p2x(15,"y"));  // 下部分を3分割
 
 	window.canv = canv;
+
+	const anim = async (before, after, isR2L=true)=>{
+		const outW = (isR2L?(-window.outerWidth):(window.outerWidth));
+		$(before).animate({ left: outW }, 200)
+		await sleep(200);
+		$(before).removeClass("showing");
+		await sleep(600);
+		$(after).addClass("showing").css({left: -outW}).animate({left: 0},200);
+	}
+	$("div.btnContainer button#play").click(()=>{
+		anim(".container.start", ".container.select");
+	});
+	$("div.btnContainer button#news").click(()=>{
+		anim(".container.start", ".container.news");
+	});
+	$("div.btnContainer button#links").click(()=>{
+		anim(".container.start", ".container.links");
+	});
+	$("div.container header button.back").click(function(){
+		anim(`.container.${$(this).attr("data-from")}`, `.container.${$(this).attr("data-to")}`,false);
+	})
 });
