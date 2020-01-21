b301c3ed8a7f639960222aded337f7be09c25773
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-01-21 20:02:38 +0900

    Add Webpack Transpile src/

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+	resolve: {
+		modules: ['node_modules', 'es2015']
+	},
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname),
+		filename: 'bundle.js'
+	},
+	mode: "production"
+};
