1b862bca04aa16f6d00f14ee07bf6ebbc5f73337
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-02-16 09:20:44 +0900

    Update .gitignore, webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,12 @@ module.exports = {
 	resolve: {
 		modules: ['node_modules', 'es2015']
 	},
+	target: 'node',
 	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname),
 		filename: 'bundle.js'
 	},
+	// mode: "development",
 	mode: "production"
 };
